42ac8be5ae0b03fc56627e81ce898b19adc2aaf9
17saumyatripathi <17saumyatripathi@gmail.com>
2025-04-21 12:57:39 +0530

    Live

--- day4/frontend/src/components/Delete.jsx
+++ day4/frontend/src/components/Delete.jsx
@@ -7,7 +7,7 @@ const Delete = () => {
     const id = e.target.id.value;
     
     try {
-      await axios.delete(`http://localhost:9000/users/${id}`);
+      await axios.delete(`https://fsdbackend-fwxi.onrender.com/users/${id}`);
       alert('User deleted successfully');
     } catch (error) {
       console.error("Error deleting user:", error);
